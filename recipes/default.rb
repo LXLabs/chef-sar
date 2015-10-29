a16f0dc32a57346890b8d8c5dd89fbe3b1aacfd4
Ryan Murphy <Ryan.Murphy@havasww.com>
2015-10-29 10:54:20 +0000

    changed back

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,21 +2,6 @@ package 'sysstat' do
   action :install
 end
 
-case node[:platform]
-  when "ubuntu", "debian"
-template '/etc/sysstat/sysstat' do
-  source 'sysstat.options.erb'
-  mode 0644
-  owner 'root'
-  group 'root'
-  variables(
-    sar_history: node['sar']['options']['sar_history'],
-    sdac_options: node['sar']['options']['sdac_options'],
-    sar_compress: node['sar']['options']['sar_compress']
-  )
-  end
-end
-end
 
 template '/etc/default/sysstat' do
   source 'sysstat.default.erb'
