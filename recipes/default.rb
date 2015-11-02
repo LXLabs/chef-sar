6fd5b663025b2d403409d4ffb5e363cbd0a319c2
Ryan Murphy <Ryan.Murphy@havasww.com>
2015-11-02 13:00:55 +0000

    adding history options

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,19 @@ package 'sysstat' do
   action :install
 end
 
+template '/etc/sysstat/sysstat'
+  source 'sysstat.options.erb'
+  mode 0644
+  owner 'root'
+  group 'root'
+  variables(
+
+  sar_history: node['sar']['options']['sar_history'],
+  sdac_options: node['sar']['options']['sdac_options'],
+  sar_compress: node['sar']['options']['sar_compress']
+
+  )
+end
 
 template '/etc/default/sysstat' do
   source 'sysstat.default.erb'
