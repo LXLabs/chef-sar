1622aa6d2271f2ff9d51f9eaec1d6fa8d605fe26
Ryan Murphy <Ryan.Murphy@havasww.com>
2015-11-02 14:46:38 +0000

    taking quotes out

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,4 +6,4 @@ default['sar']['cron']['run_every_minutes'] = '2'
 
 default['sar']['options']['sar_history'] = '30'
 default['sar']['options']['sdac_options'] = '-S DISK'
-default['sar']['options']['sar_compress'] = '10'
+default['sar']['options']['sar_compress'] = 10
